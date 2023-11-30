c4437443e8791be8f29b9258eba14e1921dbf215
Sulfi21 <148299879+Sulfi21@users.noreply.github.com>
2023-11-30 15:05:30 +0800

    Add files via upload

---
+++ Tumbuhann.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tumbuhann;
+public class Tumbuhann {
+    String jenis;
+    void fotosintesis(){
+        System.out.println("Proses Fototsintesis pada" + jenis);
+    }
+}
+    
+
